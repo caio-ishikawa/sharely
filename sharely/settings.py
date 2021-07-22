6cea0ed7e2dbdf1c488efcb149a74e9ddc5eeb55
caio-ishikawa <61653575+caio-ishikawa@users.noreply.github.com>
2021-07-22 16:24:08 +0100

    debug false

--- sharely/settings.py
+++ sharely/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yq2+xaxew-k+&k6b0vqov8140&8@%8e$anadl_au-p39nvzpx%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'caioishikawa98.pythonanywhere.com']
 
